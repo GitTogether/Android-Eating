dee6cad1a6cc8086d60d25e11cf0aebc50144c52
tszhangjunqiao <tszhangjunqiao@163.com>
2016-04-18 11:19:09 +0800

    add source code

---
+++ app/src/main/java/com/example/jqzhang/eating/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.jqzhang.eating;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String[] rest = {
+            "新百味",
+            "姐妹面馆",
+            "雪花面馆",
+            "龙禧",
+            "高祖",
+            "砂锅",
+            "顺旺基",
+            "倪家大食堂",
+            "七大碗",
+            "火红年代",
+            "负一楼",
+            "如意馄饨"};
+    private TextView textView;
+    private Button rand;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        rand = (Button)findViewById(R.id.rand_button);
+        rand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random random = new Random(System.currentTimeMillis());
+                int size = rest.length;
+                int index = random.nextInt(size);
+                Log.d("eatingindex", String.valueOf(index));
+                textView = (TextView)findViewById(R.id.name_textview);
+                textView.setText(rest[index]);
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
